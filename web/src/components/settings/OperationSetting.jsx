c27eabd0ff65944f00a38d6331cd051fbeb663dc
zhangwenshuai <zhangwenshuai999@gmail.com>
2026-03-12 11:26:57 +0800

    fix: 修复新开关当布尔处理

--- web/src/components/settings/OperationSetting.jsx
+++ web/src/components/settings/OperationSetting.jsx
@@ -49,6 +49,7 @@ const OperationSetting = () => {
     DefaultCollapseSidebar: false,
     DemoSiteEnabled: false,
     SelfUseModeEnabled: false,
+    SubscriptionOnlyModeEnabled: false,
 
     /* 顶栏模块管理 */
     HeaderNavModules: '',
